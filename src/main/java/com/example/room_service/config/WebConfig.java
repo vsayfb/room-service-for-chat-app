ae19bd5dd90e0fd316aab47126f91937050748ac
vsay <96926401+vsayfb@users.noreply.github.com>
2024-06-15 23:12:14 +0300

    configure cors

--- src/main/java/com/example/room_service/config/WebConfig.java
+++ src/main/java/com/example/room_service/config/WebConfig.java
@@ -1,14 +1,19 @@
 package com.example.room_service.config;
 
 import com.example.room_service.argument_resolver.ClientSessionArgumentResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed_origins:*}")
+    private String allowedOrigin;
 
     private final ClientSessionArgumentResolver clientSessionArgumentResolver;
 
@@ -19,5 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(clientSessionArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/rooms/").allowedMethods("GET", "POST").allowedOrigins(allowedOrigin);
+        registry.addMapping("/rooms/*").allowedMethods("GET").allowedOrigins(allowedOrigin);
+        registry.addMapping("/rooms/join/*").allowedMethods("POST").allowedOrigins(allowedOrigin);
     }
 }
